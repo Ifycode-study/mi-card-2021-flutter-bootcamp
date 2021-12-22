4ccf017c49186932bd93baa20ee3fc2552343d09
Ifycode <marychord@gmail.com>
2021-12-22 12:10:08 +0100

    completed business card

--- lib/main.dart
+++ lib/main.dart
@@ -11,34 +11,82 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.teal[800],
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('./images/mary-obiagba.jpeg'),
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    color: Colors.green,
-                  ),
-                ],
+              Text(
+                'Mary Obiagba',
+                style: TextStyle(
+                  color: Colors.yellow[50],
+                  fontFamily: 'Pacifico',
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
+              Text(
+                'Flutter Developer'.toUpperCase(),
+                style: TextStyle(
+                  color: Colors.yellow[200],
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 18.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.yellow[50],
+                ),
+              ),
+              Card(
+                color: Colors.yellow[50],
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[800],
+                  ),
+                  title: Text(
+                    '+234 813 366 200',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.yellow[50],
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal[800],
+                  ),
+                  title: Text(
+                    'obiagba.mary.ifeoma@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
@@ -47,3 +95,34 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+/* Container way before changing to Card widget
+
+Container(
+  color: Colors.yellow[50],
+  margin: EdgeInsets.symmetric(
+  vertical: 10.0,
+  horizontal: 25.0,
+),
+padding: EdgeInsets.all(10.0),
+child: Row(
+      children: <Widget>[
+        Icon(
+          Icons.phone,
+          color: Colors.teal[800],
+        ),
+        SizedBox(
+          width: 10.0,
+        ),
+        Text(
+          '+234 813 366 200',
+          style: TextStyle(
+          color: Colors.teal[900],
+          fontFamily: 'Source Sans Pro',
+          fontSize: 16.0,
+        ),
+      ),
+    ],
+  ),
+),
+*/
